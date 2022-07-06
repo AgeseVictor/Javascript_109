5d674d837c18dc0e9284f81225e8f2a3ae127b76
Victor Agese <101999701+AgeseVictor@users.noreply.github.com>
2022-07-06 22:28:51 +0100

    Testing Darkmood

--- index109.js
+++ index109.js
@@ -3,4 +3,11 @@ code.classList.add("code")
 
 var con = document.getElementById("con");
 
-con.innerHTML = ("grace is poured on my Lips");
+
+// var bodi = document.getElementsByTagName("body");
+var btnn = document.getElementById("btnn");
+btnn.addEventListener("click",function(){
+    document.body.classList.toggle("purple");
+    // document.body.style.backgroundColor= "red"
+    // alert("grace")
+})
