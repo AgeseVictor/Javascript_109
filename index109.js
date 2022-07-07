afc98ecf61cb8683eb38e3f7a72cbfdd4dc857be
Victor Agese <101999701+AgeseVictor@users.noreply.github.com>
2022-07-07 11:15:32 +0100

    added some contact info

--- index109.js
+++ index109.js
@@ -1,13 +1,8 @@
-var code = document.getElementById("insight");
-code.classList.add("code")
-
-var con = document.getElementById("con");
-
-
-// var bodi = document.getElementsByTagName("body");
+// // var bodi = document.getElementsByTagName("body");
 var btnn = document.getElementById("btnn");
 btnn.addEventListener("click",function(){
     document.body.classList.toggle("purple");
+    
     // document.body.style.backgroundColor= "red"
-    // alert("grace")
+//     // alert("grace")
 })
